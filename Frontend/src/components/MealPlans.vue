60a27ef65f3f0a9804a5607ddc538904ea9a8f66
karanja <karanjavictor389@gmail.com>
2024-08-07 16:23:45 +0300

    Styled the chevrons for daily navigation

---
+++ Frontend/src/components/MealPlans.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+<template>
+    <div>
+        <h1 class="font-bold text-xl p-4">TODAY'S MEAL PLAN</h1>
+        <section class="flex flex-col md:flex-row container mx-auto space-y-4 md:space-y-0 md:space-x-4">
+            <div class="block border rounded p-2 md:space-x-2 shadow-lg md:w-1/4">
+                <h1 class="font-bold p-2 text-lime-500">Breakfast</h1>
+                <p>Grilled Chicken Salad</p>
+            </div>
+            <div class="block border rounded p-2 md:space-x-2 shadow-lg md:w-1/4">
+                <h1 class="font-bold p-2 text-rose-500">Lunch</h1>
+                <p>Grilled Chicken Salad</p>
+            </div>
+            <div class="block border rounded p-2 md:space-x-2 shadow-lg md:w-1/4">
+                <h1 class="font-bold p-2 text-sky-500">Supper</h1>
+                <p>Grilled Chicken Salad</p>
+            </div>
+            <div class="block border rounded p-2 md:space-x-2 shadow-lg md:w-1/4">
+                <h1 class="font-bold p-2 text-lime-500">Snack</h1>
+                <p>Grilled Chicken Salad</p>
+            </div>
+        </section>
+    </div>
+</template>
--- Frontend/src/components/Workouts.vue
+++ Frontend/src/components/Workouts.vue
@@ -1,7 +1,8 @@
-<script setup></script>
+<script setup>
+import MealPlans from '@/components/MealPlans.vue'</script>
 <template>
     <section class="container mx-auto p-4 block  mt-10 space-y-4 mb-20">
-        <div class="block">
+        <div class="block relative">
             <div class="flex flex-col lg:flex-row space-x-4 p-4">
                 <img src="/images/2150404901.jpg" alt="" class="object-fit lg:w-1/2">
                 <div class="block p-4">
@@ -35,9 +36,17 @@
 
                 </div>
             </div>
-            <div>
-                <h1 class="font-bold text-xl px-4">TODAY'S MEAL PLAN</h1>
-            </div>
+            <MealPlans />
+            <span class="absolute left-2 top-2 flex items-center justify-center z-50 cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-20 text-black bg-blue-200 opacity-80 rounded-full border">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                </svg>
+            </span>
+            <span class="absolute right-2 top-2 flex items-center justify-center z-50 cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-20 text-black bg-blue-200 opacity-80 rounded-full border">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+            </span>
         </div>
     </section>
 
